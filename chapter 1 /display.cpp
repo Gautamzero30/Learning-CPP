5341b7be198e33be54a4b24dd9d820d73f3d2be4
Gautamzero30 <gautamcse357@gmail.com>
2025-07-24 18:17:31 +0545

    first one

---
+++ chapter 1 /display.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaring variables
+    int a, b, sum;
+
+    // Taking input
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    // Performing addition
+    sum = a + b;
+
+    // Displaying output
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
+
+    return 0;
+}
